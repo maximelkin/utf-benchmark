30e953d13e20e1acf6f7834aede9fb03c71faacf
maxeljkin <maxeljkin@gmail.com>
2022-05-07 04:54:43 +0300

    reformat

--- src/main.cpp
+++ src/main.cpp
@@ -3,28 +3,28 @@
 #include <node_api.h>
 #include <memory>
 
-Napi::Value Decode(const Napi::CallbackInfo& info) {
+Napi::Value Decode(const Napi::CallbackInfo &info) {
   Napi::Env env = info.Env();
   if (info.Length() != 1) {
-      Napi::TypeError::New(env, "Wrong number of arguments")
-          .ThrowAsJavaScriptException();
+    Napi::TypeError::New(env, "Wrong number of arguments")
+            .ThrowAsJavaScriptException();
 
-      return env.Null();
+    return env.Null();
   }
-  auto buf = info[0].As<Napi::Buffer<char>>();
+  auto buf = info[0].As < Napi::Buffer < char >> ();
 
   return Napi::String::New(env, buf.Data(), buf.Length());
 }
 
-Napi::Value DecodeSimd(const Napi::CallbackInfo& info) {
+Napi::Value DecodeSimd(const Napi::CallbackInfo &info) {
   Napi::Env env = info.Env();
   if (info.Length() != 1) {
-      Napi::TypeError::New(env, "Wrong number of arguments")
-          .ThrowAsJavaScriptException();
+    Napi::TypeError::New(env, "Wrong number of arguments")
+            .ThrowAsJavaScriptException();
 
-      return env.Null();
+    return env.Null();
   }
-  auto buf = info[0].As<Napi::Buffer<char>>();
+  auto buf = info[0].As < Napi::Buffer < char >> ();
 
 
   size_t expected_utf16words = simdutf::utf16_length_from_utf8(buf.Data(), buf.Length());
@@ -35,15 +35,15 @@ Napi::Value DecodeSimd(const Napi::CallbackInfo& info) {
 
     napi_value result;
     auto status = napi_create_string_latin1(env, buf.Data(), buf.Length(), &result);
-      if (status != napi_ok) {
-            Napi::TypeError::New(env, "create string failed")
-                .ThrowAsJavaScriptException();
-            return env.Null();
-      }
-      return Napi::Value(env, result);
+    if (status != napi_ok) {
+      Napi::TypeError::New(env, "create string failed")
+              .ThrowAsJavaScriptException();
+      return env.Null();
+    }
+    return Napi::Value(env, result);
   }
 
-  std::unique_ptr<char16_t[]> utf16_output{new char16_t[expected_utf16words]};
+  std::unique_ptr < char16_t[] > utf16_output{new char16_t[expected_utf16words]};
 
   size_t utf16words = simdutf::convert_utf8_to_utf16(buf.Data(), buf.Length(), utf16_output.get());
 
@@ -52,13 +52,13 @@ Napi::Value DecodeSimd(const Napi::CallbackInfo& info) {
 
   if (status != napi_ok) {
     Napi::TypeError::New(env, "create string failed")
-        .ThrowAsJavaScriptException();
+            .ThrowAsJavaScriptException();
 
     return env.Null();
   }
 
 
-      return Napi::Value(env, result);
+  return Napi::Value(env, result);
 }
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
